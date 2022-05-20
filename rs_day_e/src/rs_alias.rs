dd733f9375454c14f2ebd98500d14e024c472b58
tianxueren <1770664399@qq.com>
2022-05-20 21:47:57 +0800

    Rust daliy5 补齐

--- rs_day_e/src/rs_alias.rs
+++ rs_day_e/src/rs_alias.rs
@@ -1,3 +1,31 @@
 #![allow(dead_code)]
+
+// `NanoSecond` 是 `u64` 的新名字。
+type NanoSecond = u64;
+type Inch = u64;
+
+type Alias = NkAlias;
+
+#[derive(Debug)]
+struct NkAlias {
+    x: u8,
+}
+
+// 通过这个属性屏蔽警告。
+#[allow(non_camel_case_types)]
+type u64_t = u64;
 #[test]
-fn test_alias() {}
+fn test_alias() {
+    let nanoseconds: NanoSecond = 5 as u64_t;
+    let inches: Inch = 2 as u64_t;
+
+    // 注意类型别名*并不能*提供额外的类型安全，因为别名*并不是*新的类型。
+    println!(
+        "{} nanoseconds + {} inches = {} unit?",
+        nanoseconds,
+        inches,
+        nanoseconds + inches
+    );
+
+    println!("{:?}", Alias { x: 10 })
+}
